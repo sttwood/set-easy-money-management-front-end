d1b2491631390eb78dfd25c836edb7adb376dc9f
Settawut Borrisut <settawut.b@nexilar.com>
2024-04-06 23:45:33 +0700

    fix: import authOption at profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,6 +1,6 @@
 import {getServerSession} from 'next-auth'
 import React from 'react'
-import {authOptions} from '../api/auth/[...nextauth]/route'
+import {authOptions} from '@/lib/authOptions'
 import Image from 'next/image'
 import {redirect} from 'next/navigation'
 
