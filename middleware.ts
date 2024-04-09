06569ed21af8a38e8e7a230e447ec88799f06bfa
Settawut Borrisut <settawut.b@nexilar.com>
2024-04-09 11:28:01 +0700

    chore: remove comment

--- middleware.ts
+++ middleware.ts
@@ -3,9 +3,6 @@ import {withAuth} from 'next-auth/middleware'
 export default withAuth({
   callbacks: {
     authorized: async ({req, token}: {req: any, token: any}) => {
-      // if (req.nextUrl.pathname.startsWith('/')) {
-      //   return '/dashboard';
-      // }
       if (req.nextUrl.pathname.startsWith('/admin')) {
         return token?.role === 'admin'
       }
